0eb190bae83676338935ea603159590bc4223525
sujondev <emd26521@gmail.com>
2022-09-15 15:11:09 +0600

    mistake count add

--- script.js
+++ script.js
@@ -46,6 +46,7 @@ const typeController = (e) => {
     display.innerHTML += `<span class="green">${newLetter === " " ? "▪" : newLetter}</span>`;
   } else {
     display.innerHTML += `<span class="red">${newLetter === " " ? "▪" : newLetter}</span>`;
+    errorCount++
   }
 
   // check if given question text is equal to user typed text
@@ -60,6 +61,8 @@ const validate = (key) => {
   }
   return false;
 };
+
+
 
 // FINISHED TYPING
 const gameOver = () => {
